5b194f378030026b852e240524d57d8ac15988e4
nyacho04 <dnachod83@gmail.com>
2025-02-21 11:33:11 -0300

    spin words fix

--- spin_words.py
+++ spin_words.py
@@ -4,8 +4,8 @@ def spin_words(sentence):
     listy = sentence.split()
 
     for word in listy:
-        if len(word)>= 5:
-            word[i] = word[i][::-1]
+        if len(word) >= 5:
+            word = word[::-1]
         result.append(word)
     
     return ' '.join(result)
